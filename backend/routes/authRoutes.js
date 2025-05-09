bdce0730aff9d5b698cc8f52a54bf2bacc7125ca
nightsky18 <mariana_montoya82221@elpoli.edu.co.unlinked202406092302>
2025-05-09 12:11:46 -0500

    HU-1

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,10 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const router = express.Router();
-const authController = require('../controllers/authController');
 
-router.post('/register', authController.register);
-router.get('/verify-email/:token', authController.verifyEmail);
 
 
 
